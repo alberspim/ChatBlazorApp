18bacd016894968d3468d8c03717187780ee943e
Pim Albers <pimboa@hotmail.com>
2021-09-15 13:44:00 +0200

    Initial Code commit

---
+++ ChatBlazorApp/Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace ChatBlazorApp.Server
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CreateHostBuilder(System.Array.Empty<string>()).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
