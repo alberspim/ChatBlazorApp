f438e3b098ad6e0607a2ec952685da61145069be
alberspim <pimboa@hotmail.com>
2021-09-15 15:16:27 +0200

    Add credential handler

---
+++ ChatBlazorApp/Client/IncludeRequestCredentialsMessagHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatBlazorApp.Client
+{
+    public class IncludeRequestCredentialsMessagHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
